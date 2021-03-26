30e922d736bfed6c457b2e14a3323f7b1a9fa3c1
Sergio Flores <relfos@gmail.com>
2021-03-26 16:51:24 +0000

    Added current files

---
+++ Assets/Phantasma/Scripts/PhantasmaLinkClient.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NativeWebSocket;
+using UnityEngine;
+using UnityEngine.UI;
+using LunarLabs.Parser;
+using LunarLabs.Parser.JSON;
+using Phantasma.Numerics;
+using Phantasma.Cryptography;
+
+public class PhantasmaLinkClient: MonoBehaviour
+{
+    public struct Balance 
+    {
+        public readonly string symbol;
+        public readonly BigInteger value;
+        public readonly int decimals;
+
+        public Balance(string symbol, BigInteger value, int decimals)
+        {
+            this.symbol = symbol;
+            this.value = value;
+            this.decimals = decimals;
+        }
+    }
+
+    public static PhantasmaLinkClient Instance { get; private set; }
+
+    public string DappID = "demo";
+
+    public string Host = "localhost:7090";
+    public string Nexus = "simnet";
+
+    public int GasPrice = 100000;
+
+    public CanvasGroup ModalTarget;
+
+    private WebSocket websocket;
+
+    private Text _messageObj;
+
+    public bool Ready { get; private set; }
+
+    public bool Enabled { get; private set; }
+
+    public bool Busy { get; private set; }
+    
+    public string Wallet { get; private set; }
+    public string Token { get; private set; }
+    public string Name { get; private set; }
+    public string Address { get; private set; }
+
+    public Texture2D Avatar { get; private set; }
+
+    public IEnumerable<string> Assets => _balanceMap.Keys;
+
+    private Dictionary<int, Action<DataNode>> _requestCallbacks = new Dictionary<int, Action<DataNode>>();
+
+    private Dictionary<string, Balance> _balanceMap = new Dictionary<string, Balance>();
+
+    void Awake()
+    {
+        Instance = this;
+
+        var bgObj = ModalTarget.GetComponent<RectTransform>();
+        bgObj.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+        var temp = ModalTarget.transform.Find("Message");
+        _messageObj = temp.GetComponent<Text>();
+        _messageObj.text = "Loading...";
+
+        var rt = temp.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(Screen.width, Screen.height);
+
+        SetModal(false);
+    }
+
+    public async void Enable()
+    {
+        if (Enabled)
+        {
+            return;
+        }
+
+        this.Enabled = true;
+
+        if (ModalTarget == null)
+        {
+            Debug.LogError("Please assign Modal target in inspector...");
+            return;
+        }
+
+        this.Wallet = "Unknown";
+        this.Token = null;
+
+        websocket = new WebSocket($"ws://{Host}/phantasma");
+
+        websocket.OnOpen += () =>
+        {
+            this.Ready = true;
+            Debug.Log("Connection open!");
+        };
+
+        websocket.OnError += (e) =>
+        {
+            Debug.Log("Error! " + e);
+        };
+
+        websocket.OnClose += (e) =>
+        {
+            Debug.Log("Connection closed!");
+        };
+
+        websocket.OnMessage += (bytes) =>
+        {
+            // getting the message as a json string
+            var json = System.Text.Encoding.UTF8.GetString(bytes);
+            Debug.Log("OnMessage! " + json);
+
+            var node = JSONReader.ReadFromString(json);
+
+            var reqID = node.GetInt32("id");
+            if (_requestCallbacks.ContainsKey(reqID))
+            {
+                var callback = _requestCallbacks[reqID];
+                _requestCallbacks.Remove(reqID);
+
+                callback(node);
+            }
+            else
+            {
+                Debug.LogWarning("Got weird request with id " + reqID);
+            }
+        };
+
+        // waiting for messages
+        await websocket.Connect();
+    }
+
+    void Update()
+    {
+        if (!Enabled)
+        {
+            return;
+        }
+
+#if !UNITY_WEBGL || UNITY_EDITOR
+        websocket?.DispatchMessageQueue();
+#endif
+    }
+
+    /*async void SendWebSocketMessage()
+    {
+        if (websocket.State == WebSocketState.Open)
+        {
+            // Sending bytes
+            await websocket.Send(new byte[] { 10, 20, 30 });
+
+            // Sending plain text
+            await websocket.SendText("plain text message");
+        }
+    }*/
+
+    private async void OnApplicationQuit()
+    {
+        if (websocket != null)
+        {
+            await websocket.Close();
+        }
+    }
+
+    private void FetchAccount(Action<bool, string> callback)
+    {
+        ShowModal("Authorized, obtaining account info...");
+
+        SendLinkRequest("getAccount", (result) =>
+        {
+            HideModal();
+
+            var success = result.GetBool("success");
+            if (success)
+            {
+                var avatarData = result.GetString("avatar");
+                avatarData = avatarData.Split(new char[] { ',' }, 2)[1];
+
+                var avatarBytes = System.Convert.FromBase64String(avatarData);
+
+                this.Avatar = new Texture2D(32, 32, TextureFormat.ARGB32, false, true);                
+                ImageConversion.LoadImage(this.Avatar, avatarBytes);
+
+                Debug.Log($"Avatar: {Avatar.width}x{Avatar.height}");
+
+                this.Name = result.GetString("name");
+                this.Address = result.GetString("address");
+
+                _balanceMap.Clear();
+
+                var balances = result.GetNode("balances");
+                if (balances != null)
+                {
+                    foreach (var child in balances.Children)
+                    {
+                        var symbol = child.GetString("symbol");
+                        var value = child.GetString("value");
+                        var decimals = child.GetInt32("decimals");
+
+                        var amount = BigInteger.Parse(value);
+                        _balanceMap[symbol] = new Balance(symbol, amount, decimals);
+                    }
+                }
+
+                callback(true, null);
+            }
+            else
+            {
+                callback(false, "could not obtain account");
+            }
+        });
+    }
+
+    private int requestID;
+
+    private void SetModal(bool enabled)
+    {
+        Busy = enabled;
+        ModalTarget.alpha = enabled ? 1: 0;
+    }
+
+    private void ShowModal(string msg)
+    {
+        _messageObj.text = msg;
+        SetModal(true);
+    }
+
+    private void HideModal()
+    {
+        SetModal(false);
+    }
+
+    private async void SendLinkRequest(string request, Action<DataNode> callback)
+    {
+        if (this.Token != null)
+        {
+            request = request + '/' + this.DappID + '/' + this.Token;
+        }
+
+        requestID++;
+
+        request = $"{requestID},{request}";
+
+        _requestCallbacks[requestID] = callback;
+        Debug.Log("Request=>" + request);
+
+        await websocket.SendText(request);
+    }
+
+    #region PUBLIC INTERFACE
+
+    public decimal GetBalance(string symbol)
+    {
+        if (_balanceMap.ContainsKey(symbol))
+        {
+            var temp = _balanceMap[symbol];
+            return UnitConversion.ToDecimal(temp.value, temp.decimals);
+        }
+
+        return 0;
+    }
+
+    public void Login(Action<bool, string> callback)
+    {
+        if (string.IsNullOrEmpty(this.Nexus))
+        {
+            ShowModal("Nexus is not setup correctly...");
+            return;
+        }
+
+        ShowModal("Connection established, authorizing...");
+
+        SendLinkRequest($"authorize/{DappID}", (result) =>
+        {
+            var success = result.GetBool("success");
+            if (success)
+            {
+                var connectedNexus = result.GetString("nexus");
+
+                if (connectedNexus != this.Nexus)
+                {
+                    HideModal();
+                    callback(false, $"invalid nexus: got {connectedNexus} but expected {this.Nexus}");
+                }
+                else
+                {
+                    this.Wallet = result.GetString("wallet");
+                    this.Token = result.GetString("token");
+
+                    FetchAccount(callback);
+                }
+            }
+            else
+            {
+                HideModal();
+                callback(false, "connection failed (or rejected)");
+            }
+        });
+    }
+
+    public void SendTransaction(string chain, byte[] script, byte[] payload, Action<Hash, string> callback)
+    {
+        ShowModal("Relaying transaction...");
+
+        if (script.Length >= 8192)
+        {
+            callback(Hash.Null, "script too big");
+            return; 
+        }
+
+        var hexScript = Base16.Encode(script);
+        var hexPayload = payload != null && payload.Length > 0 ?  Base16.Encode(payload) : ""; // is empty string for payload ok?
+
+        SendLinkRequest($"signTx/{this.Nexus}/{chain}/{hexScript}/{hexPayload}", (result) =>
+        {
+            HideModal();
+            var success = result.GetBool("success");
+            if (success)
+            {
+                var hashStr = result.GetString("hash");
+                var hash = Hash.Parse(hashStr);
+                callback(hash, null);
+            }
+            else
+            {
+                var msg = result.GetString("message");
+                callback(Hash.Null, "transaction rejected: "+ msg);
+            }
+        });
+    }
+
+    #endregion
+}
---
+++ Assets/Phantasma/Scripts/WebClient.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+using LunarLabs.Parser;
+using LunarLabs.Parser.JSON;
+using System.Text;
+
+namespace Phantasma.SDK
+{
+    public static class WebClient
+    {
+        public static int NoTimeout = 0;
+        public static int DefaultTimeout = 30;
+        private static long requestNumber = 0;
+        private static object requestNumberLock = new object();
+        private static long GetNextRequestNumber()
+        {
+            lock (requestNumberLock)
+            {
+                if (requestNumber == Int64.MaxValue)
+                    requestNumber = 0;
+                else
+                    requestNumber++;
+            }
+
+            return requestNumber;
+        }
+        public static IEnumerator RPCRequest(string url, string method, int timeout, Action<EPHANTASMA_SDK_ERROR_TYPE, string> errorHandlingCallback,
+                                            Action<DataNode> callback, params object[] parameters)
+        {
+            var paramData = DataNode.CreateArray("params");
+
+            if (parameters != null && parameters.Length > 0)
+            {
+                foreach (var obj in parameters)
+                {
+                    paramData.AddField(null, obj);
+                }
+            }
+
+            var jsonRpcData = DataNode.CreateObject(null);
+            jsonRpcData.AddField("jsonrpc", "2.0");
+            jsonRpcData.AddField("method", method);
+            jsonRpcData.AddField("id", "1");
+            jsonRpcData.AddNode(paramData);
+
+            UnityWebRequest request;
+            string json;
+
+            try
+            {
+                json = JSONWriter.WriteToString(jsonRpcData);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            var requestNumber = GetNextRequestNumber();
+            Log.Write($"RPC request [{requestNumber}]\nurl: {url}\njson: {json}", Log.Level.Networking);
+
+            request = new UnityWebRequest(url, "POST");
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            DateTime startTime = DateTime.Now;
+
+            if(timeout > 0)
+                request.timeout = timeout;
+            
+            yield return request.SendWebRequest();
+            
+            TimeSpan responseTime = DateTime.Now - startTime;
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Log.Write($"RPC error [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.error}\nisNetworkError: {request.isNetworkError}\nisHttpError: {request.isHttpError}\nresponseCode: {request.responseCode}", Log.Level.Networking);
+                if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.WEB_REQUEST_ERROR, request.error + $"\nURL: {url}\nIs network error: {request.isNetworkError}\nIs HTTP error: {request.isHttpError}\nResponse code: {request.responseCode}");
+            }
+            else
+            {
+                Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.downloadHandler.text}", Log.Level.Networking);
+                DataNode root = null;
+
+                try
+                {
+                    root = JSONReader.ReadFromString(request.downloadHandler.text);
+                }
+                catch (Exception e)
+                {
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nFailed to parse JSON: " + e.Message, Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.FAILED_PARSING_JSON, "Failed to parse JSON: " + e.Message);
+                    yield break;
+                }
+
+                if (root == null)
+                {
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nFailed to parse JSON", Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.FAILED_PARSING_JSON, "failed to parse JSON");
+                }
+                else
+                if (root.HasNode("error"))
+                {
+                    var errorDesc = root["error"].GetString("message");
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nError node found: {errorDesc}", Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.API_ERROR, errorDesc);
+                }
+                else
+                if (root.HasNode("result"))
+                {
+                    var result = root["result"];
+
+                    if (result.HasNode("error"))
+                    {
+                        // This is incorrect way of RPC error reporting,
+                        // but it happens sometimes and should be handeled at least for now.
+                        var errorDesc = result.GetString("error");
+                        Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nError node found (2): {errorDesc}", Log.Level.Networking);
+                        if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.API_ERROR, errorDesc);
+                    }
+                    else
+                    {
+                        callback(result);
+                    }
+                }
+                else
+                {
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.MALFORMED_RESPONSE, "malformed response");
+                }
+            }
+
+            yield break;
+        }
+        public static IEnumerator RPCRequestEx(string url, string method, int timeout, Action<EPHANTASMA_SDK_ERROR_TYPE, string> errorHandlingCallback,
+            Action<DataNode> callback, DataNode parametersNode)
+        {
+            var jsonRpcData = DataNode.CreateObject(null);
+            jsonRpcData.AddField("jsonrpc", "2.0");
+            jsonRpcData.AddField("method", method);
+            jsonRpcData.AddField("id", "1");
+            jsonRpcData.AddNode(parametersNode);
+
+            UnityWebRequest request;
+            string json;
+
+            try
+            {
+                json = JSONWriter.WriteToString(jsonRpcData);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            var requestNumber = GetNextRequestNumber();
+            Log.Write($"RPC request [{requestNumber}]\nurl: {url}\njson: {json}", Log.Level.Networking);
+
+            request = new UnityWebRequest(url, "POST");
+            byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            DateTime startTime = DateTime.Now;
+
+            if (timeout > 0)
+                request.timeout = timeout;
+
+            yield return request.SendWebRequest();
+            
+            TimeSpan responseTime = DateTime.Now - startTime;
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Log.Write($"RPC error [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.error}\nisNetworkError: {request.isNetworkError}\nisHttpError: {request.isHttpError}\nresponseCode: {request.responseCode}", Log.Level.Networking);
+                if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.WEB_REQUEST_ERROR, request.error + $"\nURL: {url}\nIs network error: {request.isNetworkError}\nIs HTTP error: {request.isHttpError}\nResponse code: {request.responseCode}");
+            }
+            else
+            {
+                Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.downloadHandler.text}", Log.Level.Networking);
+                DataNode root = null;
+
+                try
+                {
+                    root = JSONReader.ReadFromString(request.downloadHandler.text);
+                }
+                catch(Exception e)
+                {
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nFailed to parse JSON: " + e.Message, Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.FAILED_PARSING_JSON, "Failed to parse JSON: " + e.Message);
+                    yield break;
+                }
+
+                if (root == null)
+                {
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nFailed to parse JSON", Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.FAILED_PARSING_JSON, "failed to parse JSON");
+                }
+                else
+                if (root.HasNode("error"))
+                {
+                    var errorDesc = root["error"].GetString("message");
+                    Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nError node found: {errorDesc}", Log.Level.Networking);
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.API_ERROR, errorDesc);
+                }
+                else
+                if (root.HasNode("result"))
+                {
+                    var result = root["result"];
+
+                    if (result.HasNode("error"))
+                    {
+                        // This is incorrect way of RPC error reporting,
+                        // but it happens sometimes and should be handeled at least for now.
+                        var errorDesc = result.GetString("error");
+                        Log.Write($"RPC response [{requestNumber}]\nurl: {url}\nError node found (2): {errorDesc}", Log.Level.Networking);
+                        if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.API_ERROR, errorDesc);
+                    }
+                    else
+                    {
+                        callback(result);
+                    }
+                }
+                else
+                {
+                    if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.MALFORMED_RESPONSE, "malformed response");
+                }
+            }
+
+            yield break;
+        }
+        public static IEnumerator RESTRequest(string url, int timeout, Action<EPHANTASMA_SDK_ERROR_TYPE, string> errorHandlingCallback, Action<DataNode> callback)
+        {
+            UnityWebRequest request;
+
+            var requestNumber = GetNextRequestNumber();
+            Log.Write($"REST request [{requestNumber}]\nurl: {url}", Log.Level.Networking);
+
+            request = new UnityWebRequest(url, "GET");
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+            DateTime startTime = DateTime.Now;
+
+            if (timeout > 0)
+                request.timeout = timeout;
+            
+            yield return request.SendWebRequest();
+            
+            TimeSpan responseTime = DateTime.Now - startTime;
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Log.Write($"REST error [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.error}\nisNetworkError: {request.isNetworkError}\nisHttpError: {request.isHttpError}\nresponseCode: {request.responseCode}", Log.Level.Networking);
+                if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.WEB_REQUEST_ERROR, request.error + $"\nURL: {url}\nIs network error: {request.isNetworkError}\nIs HTTP error: {request.isHttpError}\nResponse code: {request.responseCode}");
+            }
+            else
+            {
+                DataNode root = null;
+                try
+                {
+                    Log.Write($"REST response [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.downloadHandler.text}", Log.Level.Networking);
+                    root = JSONReader.ReadFromString(request.downloadHandler.text);
+                }
+                catch (Exception e)
+                {
+                    Log.Write(e.Message);
+                }
+                callback(root);
+            }
+
+            yield break;
+        }
+
+        public static IEnumerator RESTRequest(string url, string serializedJson, Action<EPHANTASMA_SDK_ERROR_TYPE, string> errorHandlingCallback, Action<DataNode> callback)
+        {
+            UnityWebRequest request;
+
+            var requestNumber = GetNextRequestNumber();
+            Log.Write($"REST request (POST) [{requestNumber}]\nurl: {url}", Log.Level.Networking);
+
+            request = new UnityWebRequest(url, "POST");
+
+            byte[] data = Encoding.UTF8.GetBytes(serializedJson);
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(data);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            DateTime startTime = DateTime.Now;
+            yield return request.SendWebRequest();
+            TimeSpan responseTime = DateTime.Now - startTime;
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Log.Write($"REST error [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.error}\nisNetworkError: {request.isNetworkError}\nisHttpError: {request.isHttpError}\nresponseCode: {request.responseCode}", Log.Level.Networking);
+                if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.WEB_REQUEST_ERROR, request.error + $"\nURL: {url}\nIs network error: {request.isNetworkError}\nIs HTTP error: {request.isHttpError}\nResponse code: {request.responseCode}");
+            }
+            else
+            {
+                DataNode root = null;
+                try
+                {
+                    Log.Write($"REST response [{requestNumber}]\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.downloadHandler.text}", Log.Level.Networking);
+                    root = JSONReader.ReadFromString(request.downloadHandler.text);
+                }
+                catch(Exception e)
+                {
+                    Log.Write(e.Message);
+                }
+                callback(root);
+            }
+
+            yield break;
+        }
+
+        public static IEnumerator Ping(string url, Action<EPHANTASMA_SDK_ERROR_TYPE, string> errorHandlingCallback, Action<TimeSpan> callback)
+        {
+            UnityWebRequest request;
+
+            Log.Write($"Ping url: {url}", Log.Level.Networking);
+
+            request = new UnityWebRequest(url, "GET");
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+
+            DateTime startTime = DateTime.Now;
+            yield return request.SendWebRequest();
+            TimeSpan responseTime = DateTime.Now - startTime;
+
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Log.Write($"Ping error\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.error}\nisNetworkError: {request.isNetworkError}\nisHttpError: {request.isHttpError}\nresponseCode: {request.responseCode}", Log.Level.Networking);
+                if (errorHandlingCallback != null) errorHandlingCallback(EPHANTASMA_SDK_ERROR_TYPE.WEB_REQUEST_ERROR, request.error + $"\nURL: {url}\nIs network error: {request.isNetworkError}\nIs HTTP error: {request.isHttpError}\nResponse code: {request.responseCode}");
+            }
+            else
+            {
+                Log.Write($"Ping response\nurl: {url}\nResponse time: {responseTime.Seconds}.{responseTime.Milliseconds} sec\n{request.downloadHandler.text}", Log.Level.Networking);
+                callback(responseTime);
+            }
+
+            yield break;
+        }
+    }
+}
